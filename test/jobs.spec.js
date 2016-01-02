b188e9f1b52a3542f91afa70ffd4cea546e6d992
Mateus Freira <mateus.freira@gmail.com>
2016-01-02 13:22:16 -0200

    Transforming test.js in mocha tests

---
+++ test/jobs.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+var assert = require("assert");
+var jobs = require('../index.js');
+describe('Jobs Search tests', function () {
+  it('Search findById', function (done) {
+      var jobs = require('../index.js');
+      jobs.find({
+          term : 'Node.js',
+          location : 'San Francisco'
+      }, function(err, results){
+          assert.equal(err, null);
+          assert.notEqual(results.length, undefined);
+          var job = results[0];
+          jobs.findById(job.id, function(err, result){
+              assert.equal(err, null);
+              assert.notEqual(result.title, undefined);
+              done();
+          });
+      });
+  });
+  it('Search term with Location', function (done) {
+    jobs.find({
+      term : 'Node.js',
+      location : 'San Francisco'
+    }, function(err, results){
+      assert.equal(err, null);
+      assert.notEqual(results.length, undefined);
+      done();
+    });
+  });
+});
